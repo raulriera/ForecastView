c17395be7f8f527763558f12abd4365859dfc234
Raul Riera <rieraraul@gmail.com>
2015-08-16 14:07:31 +0100

    Improving the documentation. Also added a new "Automatic" format

--- ForecastView/ForecastView/Temperature.swift
+++ ForecastView/ForecastView/Temperature.swift
@@ -8,25 +8,52 @@
 
 import Foundation
 
+/**
+A temperature value type.
+*/
 public struct Temperature {
     let celsius: Int
     let fahrenheit: Int
     let format: TemperatureFormat
     
+    /// Temperature reading in the format specified.
     public var value: Int {
         switch format {
         case .Celsius:
             return celsius
         case .Fahrenheit:
             return fahrenheit
+        case .Automatic:
+            let isMetricSystem = NSLocale.currentLocale().objectForKey(NSLocaleUsesMetricSystem) as! Bool
+            
+            if isMetricSystem {
+                return celsius
+            } else {
+                return fahrenheit
+            }
         }
     }
     
+    /**
+    Temperature format to display to the user.
+    
+    - Automatic:	Reads the current locale to determine the best format to display
+    - Celsius:		Displays the temperature in celsius
+    - Fahrenheit:	Displays the temperature in fahrenheit
+    */
     public enum TemperatureFormat {
+        case Automatic
         case Celsius
         case Fahrenheit
     }
     
+    /**
+    Creats an instance of the temperature value type.
+    
+    - parameter celsius:	temperature reading in celsius
+    - parameter fahrenheit:	temperature reading in fahrenheit
+    - parameter format:		temperature format to display to the user
+    */
     init(celsius: Int, fahrenheit: Int, format: TemperatureFormat = .Celsius) {
         self.celsius = celsius
         self.fahrenheit = fahrenheit
